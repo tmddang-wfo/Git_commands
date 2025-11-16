bb836e9f538ddc116e9f7b8b4a391888be19b772
Dang Trinh <dang.tm@workforceoptimizer.com>
2025-11-16 20:39:14 +0700

    Modify into new function

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
+
 fn main() {
-    println!("Hello, world!");
+    let x:u32 = 10;
+    assert_eq!("u32".to_string(), type_of(&x));
+    println!("Success!")
 }
+
+fn type_of<T>(_:&T) -> String{
+    format!("{}", std::any::type_name::<T>())
+}
+
+
+
